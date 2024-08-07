d9b0b68a2e22f3019a3e2de450d39f197f4ce826
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-08-07 16:19:59 +0100

    Dynamic logic added

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'Eat no meat for a whole month!'
+    elif month == 'february':
+        challenge_text = 'Walk for at least 20 minutes every day!'
+    elif month == 'march':
+        challenge_text = 'Learn Python for at least 20 minutes everyday!'
+    else:
+        return HttpResponseNotFound('This month is not supported!')
+    return HttpResponse(challenge_text)
